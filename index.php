4e0eca88dd8c66c896b6f1abc07ace6a161cca3b
parcijal-vb <emare.dev@gmail.com>
2026-03-02 22:21:58 +0100

    Fix typo and in index.php

--- index.php
+++ index.php
@@ -3,11 +3,12 @@ require 'vendor/autoload.php';
 
 $faker = Faker\Factory::create();
 
-for($i=1;$i<=5;$i++){
+for($i = 1; $i <= 5; $i++) {
 
-echo "Ime: ".$faker->name()."<br>";
-echo "Email: ".$faker->email()."<br>";
-echo "Adresa: ".$faker->address()."<br>";
-echo "<hr>";
+    echo "Ime: " . $faker->name() . "<br>";
+    echo "Email: " . $faker->email() . "<br>";
+    echo "Adresa: " . $faker->address() . "<br>";
+    echo "<hr>";
 
+}
 ?>
