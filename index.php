2e1c42b349435249c19ecbee217620fae08d5663
parcijal-vb <emare.dev@gmail.com>
2026-03-02 21:53:18 +0100

    Add composer, replace conf file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require 'vendor/autoload.php';
+
+$faker = Faker\Factory::create();
+
+for($i=1;$i<=5;$i++){
+
+echo "Ime: ".$faker->name()."<br>";
+echo "Email: ".$faker->email()."<br>";
+echo "Adresa: ".$faker->address()."<br>";
+echo "<hr>";
+
+?>
